cbdd89bd36b18bedea2d5609a2bbe5a65ff82c0d
Peituda Metida <ph6944048@gmail.com>
2022-06-03 15:15:50 -0300

    criação de banco de dados

---
+++ pg.js
@@ -0,0 +1,17 @@
+const { Client } = require('pg');
+
+
+
+const client = new Client({
+    user: 'postgres',
+    host: 'localhost',
+    databasse: 'aula-po',
+    password: 'admin',
+    port:5432,
+})
+client.connect();
+
+client.query('SELECT NOW()', (err, res) => {
+    console.log(err, res)
+    client.end()
+});
